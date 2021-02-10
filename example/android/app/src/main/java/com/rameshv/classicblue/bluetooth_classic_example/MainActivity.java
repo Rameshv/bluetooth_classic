3cdd5c6f3593d95a4ae4ec219ecb354b3d9f26d5
Ramesh Vel <rameshvel@gmail.com>
2021-02-10 15:52:54 +0530

    Initial commit

---
+++ example/android/app/src/main/java/com/rameshv/classicblue/bluetooth_classic_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rameshv.classicblue.bluetooth_classic_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/bluetooth_classic.dart
@@ -0,0 +1,17 @@
+
+library bluetooth_classic;
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+part './BluetoothState.dart';
+part './BluetoothBondState.dart';
+part './BluetoothDeviceType.dart';
+part './BluetoothDevice.dart';
+part './BluetoothPairingRequest.dart';
+part './BluetoothDiscoveryResult.dart';
+part './BluetoothConnection.dart';
+part 'BluetoothClassic.dart';
+
